f67b78678c06b63dfcc2e2e7bd63fa9fdd153aba
CoreyOnGitHub <126117422+CoreyOnGitHub@users.noreply.github.com>
2023-06-16 09:45:15 +1200

    Add files via upload

---
+++ 00_LU_Base_v_01.py
@@ -0,0 +1,57 @@
+
+
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("please answer yes ? no")
+
+
+def instructions(): ...
+
+
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 ans 10\no"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+            # if the amount is too low / too high give
+            if low < response <= high:
+                return response
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine goes here...
+Played_before = yes_no("have you played the "
+                       "game before? ")
+
+if Played_before == "no":
+    instructions()
+
+print()
+
+# Ask user how much they want to play with...
+how_much = num_check("How much would you "
+                     "like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
